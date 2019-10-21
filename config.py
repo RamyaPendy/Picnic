636702a9378dc192db0ba0e3db65cfb206350d0c
RamyaPendy <35340784+RamyaPendy@users.noreply.github.com>
2019-10-21 11:58:22 +0200

    User credentials

--- config.py
+++ config.py
@@ -8,10 +8,6 @@ USERNAME = '###username'
 # Your Github API TOKEN
 API_TOKEN = '###api_tokem'
 PASSWORD = '####'
-# By default setting Limit to fetch no of gists of authenticated user
-# or use -L/--limit as command-line arguments
-# Github API will always return <= 30 recent gists.
-LIMIT = None
 
 BASE_URL = 'https://api.github.com'
 GIST_URL = 'https://gist.github.com'
